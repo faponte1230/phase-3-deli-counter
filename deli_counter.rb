32da5cb4486b81124399b0989bab6ee0cfa1f106
Fernando Aponte <faponte1230@yahoo.com>
2023-05-24 22:57:06 -0400

    commit tree 0b310fd818cb45b8c49a9b76d7af11916ad0f1f7

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+    line_list= []
+    if array.length < 1 
+        puts "The line is currently empty."
+    else
+        array.each_with_index  do  |value, index|
+            line_list.push("#{index + 1}. #{value}")
+        end
+        puts "The line is currently: #{line_list.join(" ")}"
+    end
+end
+
+def take_a_number(array, string)
+    array << string
+    puts "Welcome, #{string}. You are number #{array.find_index(string) + 1} in line."
+end
+
+def now_serving(array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array.shift}."
+    end
+end
